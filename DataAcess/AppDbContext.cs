3937bfa9cf51d95a096fb508e4c323126d6f2259
Hugo <frizto@gmail.com>
2024-09-17 13:23:01 -0300

    Add project files.

---
+++ DataAcess/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoIdentityUserLockout.WebAPI.DataAcess;
+
+public class AppDbContext(DbContextOptions options) : IdentityDbContext<IdentityUser>(options)
+{
+
+}
